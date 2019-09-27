8440314ea0f3d91cda9398d7a65180dd70c74d60
Tyler Starkus <tylerjstarkus@lewisu.edu>
2019-09-26 23:32:50 -0500

    possible node fix

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var express = require('express');
 const app = new express();
 
 app.get('/', function(request,response){
-    response.sendFile('index.html');
+    response.sendFile('/index.html');
 }
