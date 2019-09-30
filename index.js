d655b8bff3d7fde216bb52ce3bf13b2249172366
Tyler Starkus <tylerjstarkus@lewisu.edu>
2019-09-30 16:04:27 -0500

    remembered Procfile

--- index.js
+++ index.js
@@ -12,4 +12,8 @@ app.get('/', function(request,response){
     response.sendFile('index.html');
 });
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server running on port 3000");
+});
+
 //testing
