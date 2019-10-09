a165d7e8fe3161dfafbf557147151910efa17471
Tyler Starkus <tylerjstarkus@lewisu.edu>
2019-10-09 15:23:17 -0500

    helped Nadia fix some html/css

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-//var http = require('http');
-//
-//http.createServer(function (req, res) {
-//  res.writeHead(200, {'Content-Type': 'text/html'});
-//  res.end('Hello World!');
-//}).listen(8080);
+
 var express = require('express');
 
 const app = new express();
@@ -17,5 +12,3 @@ app.get('/', function(request,response){
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server running on port 3000");
 });
-
-//testing
