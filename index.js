c83519b7128e05602cfb4ac0e969f11da407e496
Tyler Starkus <tylerjstarkus@lewisu.edu>
2019-10-13 21:48:27 -0500

    changed html to ejs in prep for displaying posts. Also resolved related issues to the filename change

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+//jshint esversion:6
 
-var express = require('express');
+const express = require('express');
+const ejs = require("ejs");
 
 const app = new express();
+
+app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
 app.get('/', function(request,response){
-    response.sendFile(__dirname + '/index.html');
+    response.render(__dirname + '/index');
+});
+
+app.get('/campusevents', function(request,response){
+    response.render(__dirname + '/public/html/campusevents');
+});
+
+app.get('/currentevents', function(request,response){
+    response.render(__dirname + '/public/html/currentevents');
 });
 
 app.listen(process.env.PORT || 3000, function(){
