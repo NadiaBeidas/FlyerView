d933a3bb05fdd55a7b5f33a4d8ab7b93117ed21d
Tyler Starkus <tylerjstarkus@lewisu.edu>
2019-11-08 21:57:53 -0600

    lama

--- index.js
+++ index.js
@@ -3,10 +3,30 @@
 const express = require('express');
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+//eric
+var mysql = require('mysql');
 
 const app = new express();
 
 var posts = [];
+
+//eric
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "flyerView"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO posts (textOfPost, board) VALUES ('I am confusion', 'lama');";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("it is nighttime");
+    });
+  }); 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
