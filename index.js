184dda766360dd921ac8da34aaea04c86fd7a658
Tyler Starkus <tylerjstarkus@lewisu.edu>
2019-11-20 12:32:44 -0600

    finished posting

--- index.js
+++ index.js
@@ -21,11 +21,31 @@ var con = mysql.createConnection({
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    /*
     var sql = "INSERT INTO posts (textOfPost, board) VALUES ('I am confusion', 'lama');";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("it is nighttime");
-    });
+    }); */
+  });
+
+  //var grab = "SELECT textOfPost FROM posts WHERE postID = 1;"
+  var grab = "SELECT textOfPost FROM posts;"
+  con.query(grab, function(err,result,fields){
+    if (err) throw err;
+    console.log(result[0].textOfPost + " 1101");
+    test = String(result[0].textOfPost);
+    //var gtg = String(result[0].textOfPost);
+    //console.log(gtg + "what");
+  
+    for(var i=0; i <result.length; i++)
+    {
+      posts.push(result[i].textOfPost);
+    }
+    //posts.push(test + " idfk");
+    //posts.push(gtg);
+   //posts.push("this works");
+   //response.redirect("/campusevents");
   });
 
 app.set('view engine', 'ejs');
@@ -51,9 +71,33 @@ app.post('/campusevents', function(request,response){
     var post = request.body.newPost;
     console.log(post);
 
-    posts.push(post);
+    posts = [];
+    //for(var i=0; i<posts.length; i++)
+    //{
+      //posts.pop();
+      //console.log(posts);
+    //}
 
-    response.redirect("/campusevents");
+    var postTosql = "INSERT INTO posts (textOfPost, board) VALUES ('" + post + "', 'campusevents');";
+    con.query(postTosql, function(err,result){
+      if (err) throw err;
+    });
+
+    var grab = "SELECT textOfPost FROM posts;"
+    con.query(grab, function(err,result,fields){
+      if (err) throw err;
+      console.log(result[0].textOfPost + " 1101");
+      test = String(result[0].textOfPost);
+
+      for(var i=0; i <result.length; i++)
+      {
+        posts.push(result[i].textOfPost);
+      }
+      console.log(posts);
+      response.redirect("/campusevents");
+  });
+
+     //response.redirect("/campusevents");
 });
 
 app.listen(process.env.PORT || 3000, function(){
